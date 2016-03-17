872cdea9d1b06e33d8f38d14ef4fed1f50da904e
Evan Palmer <evjpalmer@gmail.com>
2016-03-17 11:49:41 -0700

    add custom validation of required fields on join page

--- js/join.js
+++ js/join.js
@@ -1,5 +1,32 @@
-function init() {
 
+function isRequired(el) {
+    return (typeof (el.required === 'boolean' && el.required) ||
+            typeof el.required === 'string') // supported in older browsers
+}
+
+function isBlank(el) {
+    return el.value === null || el.value === "";
+}
+
+function validateForm(event) {
+    // validate required fields
+    var elems = event.target.elements,
+        isValid = false;
+    for (var i = 0; i < elems.length; i++) {
+        if(isRequired(elems[i]) && isBlank(elems[i])) {
+            console.log("missing required field: ", elems[i].name);
+        }
+    }
+    if (!isValid) {
+        event.preventDefault();
+    }
+}
+
+function init() {
+    var form = document.forms[0],
+        err;
+    form.noValidate = true;
+    form.addEventListener('submit', validateForm);
 }
 
 document.addEventListener('DOMContentLoaded', init);
